da2778ad29dd5eff97b38dca30e9849d95be951c
Philip Boontje <philip.boontje@maxqanalytics.io>
2024-12-23 16:01:14 +0000

    add definitions

---
+++ models/external/ext_definitions.sql
@@ -0,0 +1,16 @@
+select
+
+        -- IDs
+        definition_id,
+
+        -- properties
+        definition_name,
+        definition_type,
+        definition_domain,
+        definition_source,
+        definition_url,
+        definition_description,
+
+FROM {{ ref('stg_gsheets__definitions') }}
+
+ORDER BY definition_id ASC
---
+++ models/staging/utilities/stg_gsheets__definitions.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source("utility", "definitions") }}
+
+),
+
+renamed as (
+
+    select
+        safe_cast(column_1 as int64) as definition_id,
+        safe_cast(column_2 as string) as definition_name,
+        safe_cast(column_3 as string) as definition_type,
+        safe_cast(column_4 as string) as definition_description,
+        safe_cast(column_5 as string) as definition_source,
+        safe_cast(column_6 as string) as definition_domain,
+        safe_cast(column_7 as string) as definition_url,
+    from source
+
+)
+
+select * from renamed
